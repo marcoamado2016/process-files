ba6fc902cdcde9d4846fc81ffc86113d64e2c456
marcoamado2016 <marcos242016@gmail.com>
2025-07-09 17:20:36 -0300

    Se agrego class-validator

--- src/compartido/validar.error.ts
+++ src/compartido/validar.error.ts
@@ -7,18 +7,18 @@ export class Validator {
   public static async validar<T>(entity: T, type: new () => T) {
     const instacia = plainToInstance(type, entity);
     const errors = await validate(instacia as any);
-    let detalleErros = [];
+    let detalleError = [];
     if (errors.length > 0) {
-      detalleErros = errors.map((e) => {
+      detalleError = errors.map((e) => {
         return {
           property: e.property,
           mesaage: e.constraints?.isString,
         };
       });
-      console.log("error", detalleErros);
+
       throw new BaseError(
         HTTP_CODES.BAD_REQUEST,
-        detalleErros.map((d) => `${d.property} => ${d.mesaage}`).join(",")
+        detalleError.map((d) => `${d.property} => ${d.mesaage}`).join(",")
       );
     }
   }
